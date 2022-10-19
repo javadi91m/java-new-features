b9243e134ebf838731d94532f762269bce54f5e5
Javadi <javadi91m@gmail.com>
2022-10-19 22:36:03 +0330

    java 11

---
+++ src/main/java/com/javadi/newfeatures/java11/script/Hello.java
@@ -0,0 +1,11 @@
+package com.javadi.newfeatures.java11.script;
+
+public class Hello {
+
+    // run this class by:
+    //      java Hello.java
+    public static void main(String[] args) {
+        System.out.println("Hello !!!!!!");
+    }
+
+}
